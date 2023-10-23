943f3516423fdaa93912bc1372e00c68b31cb1c4
ajstrongdev <adamjsalt@icloud.com>
2023-10-23 18:25:33 +0100

    upd: website

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'dracula-dark': '#282A36',
+        'dracula-pink': '#FF79C6',
+        'dracula-green': '#50FA7B',
+      },
+      backgroundImage: {
+        
+      }
+    },
+  },
+  plugins: [],
+}
