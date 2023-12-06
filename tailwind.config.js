8216d5e9c74af5cedc5e235c969dc3ce6ec83156
ajstrongdev <adamjsalt@icloud.com>
2023-12-06 12:35:17 +0000

    upd(output.css): Website redesign
    Slight redesign of the website to match my discord about me :3

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,10 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'dracula-dark': '#282A36',
+        'dracula-dark': '#6f2b40',
+        'container-dark': '#18090E',
         'dracula-pink': '#FF79C6',
-        'dracula-green': '#50FA7B',
+        'dracula-green': '#8d7be4',
       },
       backgroundImage: {
         
