4c61dc8c76ec55090b16e37df2559de02a687b39
ajstrongdev <adamjsalt@icloud.com>
2024-01-17 12:41:18 +0000

    add: Contact section

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         'dracula-pink': '#FF79C6',
         'off-white': '#c7c1c1',
         'overlay': 'rgba(0, 0, 0, 0.2);',
+        'form': 'rgba(255, 255, 255, 0.1);',
       },
       backgroundImage: {
         'banner': "url(/assets/banner.png)",
